8dae5bf0858055439085712d0985427231cba2e9
Daddesen <d.sundvall@icloud.com>
2025-04-23 10:30:15 +0200

    Menybar och lite css

---
+++ index.js
@@ -0,0 +1,9 @@
+// Hamburger toggle
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+console.log("ESP8266 site loaded!");
